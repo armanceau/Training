eeb82a9de4740bea43dd34459a6b8fec8731f512
armanceau <100920811+armanceau@users.noreply.github.com>
2023-05-27 21:06:22 +0200

    add
    Ajout de l'exercice Advice generator qui génère des conseils aléatoires à l'aide d'une api

---
+++ ws/AdviceGenerator/script.js
@@ -0,0 +1,35 @@
+function getAdvice(){
+    var request = new XMLHttpRequest();
+
+    request.open('GET','https://api.adviceslip.com/advice', true);
+    
+    request.onload = function() {
+        var data = JSON.parse(request.responseText);
+        // Afficher toutes les données de l'API
+        console.log(data);
+    
+    
+        //Récupérer la div dans l'HTML où l'on va afficher l'id de l'advice
+        var idElement = document.getElementById('id-element');
+    
+        //Injection de l'id de l'advice dans la div
+        idElement.textContent = data.slip.id;
+    
+    
+        //Récupérer la div dans l'HTML où l'on va afficher le contenu de l'advice
+        var apiDataElement = document.getElementById('data-element');
+    
+        //Injection du contenu de l'advice dans la div
+        apiDataElement.textContent = data.slip.advice;
+    }
+    request.send();
+    
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    getAdvice();
+});
+
+var reloadButton = document.getElementById('reload-button');
+reloadButton.addEventListener('click', getAdvice);
+
