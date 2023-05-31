420a6ffe30ad41ace01368a2db110b8f6c928bc0
armanceau <100920811+armanceau@users.noreply.github.com>
2023-05-31 17:46:30 +0200

    end

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 const links = document.querySelectorAll('.link');
 const imagePreview = document.getElementById('image-preview');
+const imageCaption = document.getElementById('image-caption');
 
 links.forEach(link => {
+  const image = link.dataset.image;
+  const caption = link.dataset.caption;
+
   link.addEventListener('mouseover', () => {
-    const image = link.dataset.image;
     imagePreview.src = image;
+    imageCaption.textContent = caption;
+    imagePreview.classList.add('show');
   });
 
   link.addEventListener('mouseout', () => {
     imagePreview.src = '';
+    imageCaption.textContent = '';
+    imagePreview.classList.remove('show');
   });
 });
